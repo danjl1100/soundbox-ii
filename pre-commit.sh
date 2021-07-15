96504a553bbb92dc360bb0bf24d37d9cd9d4dbb8
Daniel Lambert <danjl1100@gmail.com>
2021-07-15 13:37:38 -0500

    fixup pre-commit hook git-stashing logic

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,19 +2,33 @@
 #
 # install via:
 #    ln -s ../../pre-commit.sh .git/hooks/pre-commit
-#
-STASH_NAME="pre-commit-$(date +%s)"
-git stash save -q --keep-index "${STASH_NAME}"
 
+# First, stash index and workdir, keeping only the
+# to-be-committed changes in the working directory.
+# source: https://stackoverflow.com/a/20480591/5742216
+old_stash=$(git rev-parse -q --verify refs/stash)
+git stash save -q --keep-index "pre-commit-$(date +%s)"
+new_stash=$(git rev-parse -q --verify refs/stash)
+# If there were no changes (e.g. '--amend' or '--allow-empty')
+# then nothing was stashed, and we should skip everything,
+# including the tests themselves. (Presumably the tests passed
+# on the previous commit, so there is no need to re-run them.)
+if [ "${old_stash}" = "${new_stash}" ]; then
+  echo "pre-commit script: no changes to test"
+  exit 0
+fi
+
+# Run tests
 cargo clippy --workspace \
   && cargo test --workspace \
   && cargo doc --workspace --no-deps -q \
   && echo "Outstanding cargo fmt files:" && cargo fmt --all -- --check -l
 RESULT=$?
 
-STASHES=$(git stash list | grep "${STASH_NAME}")
-if [[ ${STASHES} != "" ]]; then
-  git stash pop -q
-fi
+# Restore changes
+# again, source: https://stackoverflow.com/a/20480591/5742216
+git reset --hard -q && git stash apply --index -q && git stash drop -q
+
+# Exit with status from test-run: nonzero prevents commit
 [ $RESULT -ne 0 ] && exit 1
 exit 0
