33cdc1d982764a1107bb51f7155568a1d0fd85e0
Daniel Lambert <danjl1100@gmail.com>
2022-04-25 11:21:47 -0500

    add flake for bin, frontend, and combined package

--- flake.nix
+++ flake.nix
@@ -5,17 +5,18 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    import-cargo.url = "github:edolstra/import-cargo";
     crate2nix = {
       url = "github:kolloch/crate2nix";
       flake = false;
     };
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
+    trunk-latest-src = {
+      url = "github:thedodd/trunk";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, crate2nix, ... }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, import-cargo, crate2nix, trunk-latest-src, ... }:
     let
       # name must match Cargo.toml
       name = "soundbox-ii";
@@ -28,10 +29,15 @@
         pkgs = import nixpkgs {
           inherit system;
           overlays = [
-            rust-overlay.overlay (self: super: {
+            rust-overlay.overlay (self: super: let
+              rust-bundle = self.rust-bin.${rustChannel}.${rustVersion}.default.override {
+                # include wasm32 for frontend compilation via trunk
+                targets = [ "wasm32-unknown-unknown" ];
+              };
+            in {
               # unpack rust-overlay's bundles to inform crate2nix
-              rustc = self.rust-bin.${rustChannel}.${rustVersion}.default;
-              cargo = self.rust-bin.${rustChannel}.${rustVersion}.default;
+              rustc = rust-bundle;
+              cargo = rust-bundle;
             })
           ];
         };
@@ -63,8 +69,67 @@
           pkgs.cargo
           pkgs.pkgconfig
         ];
+
+        # Trunk builder for frontend
+        # TODO remove this, replace with stable `pkgs` version,
+        #    once nixpkgs-unstable incorporates: https://github.com/thedodd/trunk/pull/358
+        trunk-latest = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "trunk";
+          version = "0.14.0-pre-git";
+
+          src = trunk-latest-src;
+
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = if pkgs.stdenvNoCC.isDarwin
+            then [ pkgs.libiconv pkgs.CoreServices pkgs.Security ]
+            else [ pkgs.openssl ];
+
+          # requires network
+          checkFlags = [ "--skip=tools::tests::download_and_install_binaries" ];
+
+          cargoSha256 = "sha256-DBL8fvC1pVsGsIEjNouFbeAsbH/TNfcrIrds2EWD53Q=";
+        };
+        trunkBuild = { src, buildDirRelative ? ".", pname, version }: pkgs.stdenvNoCC.mkDerivation {
+          inherit src pname version;
+
+          buildInputs = [
+            pkgs.rustc
+            # TODO change trunk-latest to stable `pkgs` version,
+            #   once nixpkgs-unstable contains pr: https://github.com/thedodd/trunk/pull/358
+            # pkgs.trunk
+            trunk-latest
+            pkgs.wasm-bindgen-cli
+            pkgs.nodePackages.sass
+            (import-cargo.builders.importCargo {
+              lockFile = "${src}/Cargo.lock";
+              inherit pkgs;
+            }).cargoHome
+          ];
+
+          buildPhase = ''
+            cd "${buildDirRelative}"
+            trunk build --dist dist
+          '';
+          installPhase = ''
+            mkdir -p "$out/share/frontend"
+            cp -r dist/* "$out/share/frontend"
+          '';
+        };
       in rec {
-        packages.${name} = project.workspaceMembers.${rootModuleName}.build;
+        packages.${name} = pkgs.symlinkJoin {
+          inherit name;
+          paths = [
+            packages."${name}_bin"
+            packages."${name}_frontend"
+          ];
+        };
+        packages."${name}_bin" = project.workspaceMembers.${rootModuleName}.build;
+        packages."${name}_frontend" = trunkBuild {
+          src = ./.;
+          buildDirRelative = "frontend";
+          pname = "${name}_frontend";
+          version = "0.1.0";
+        };
 
         # `nix build`
         defaultPackage = packages.${name};
