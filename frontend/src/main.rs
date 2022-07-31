8fed8f2d1304804662cda11c3af92148acd932e7
Daniel Lambert <danjl1100@gmail.com>
2022-07-31 11:05:00 -0500

    frontend add log-error expand view, simplify websocket reader logic

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -31,7 +31,7 @@
 #![deny(rustdoc::broken_intra_doc_links)]
 
 use backoff::{exponential::ExponentialBackoff, SystemClock};
-use macros::UpdateDelegate;
+use gloo_net::websocket::WebSocketError;
 use yew::{html, Component, Context, Html};
 use yew_router::{
     prelude::{Link, Redirect},
@@ -80,54 +80,158 @@ derive_wrapper! {
     enum AppMsg for App {
         WebSocket(websocket::Msg) for self.websocket,
         Reconnect(reconnect::Msg) for self.reconnect,
-        //User(MsgUser) for self.user_data,
-        Logger(MsgLog) for self.logger,
+        Logger(log::Msg) for self.logger,
     }
 }
-enum MsgLog {
-    Message(String),
-}
-struct Logger;
-impl UpdateDelegate<App> for Logger {
-    type Message = MsgLog;
+mod log {
+    use yew::{html, Component, Context, Html};
 
-    fn update(
-        &mut self,
-        _ctx: &yew::Context<App>,
-        MsgLog::Message(message): Self::Message,
-    ) -> bool {
-        log!("App got message {}", message);
-        false
+    use crate::macros::UpdateDelegate;
+
+    shared::wrapper_enum! {
+        pub(crate) enum Msg {
+            ClearErrors(ClearErrors),
+            { impl None for }
+            Message(String),
+            Error((&'static str, String)),
+            ShowErrorDetails(bool),
+        }
+    }
+    pub struct ClearErrors {
+        _seal: (),
+    }
+    #[derive(Default)]
+    pub(crate) struct Logger {
+        errors: Vec<LogErrorEntry>,
+        show_error_details: bool,
+    }
+    impl Logger {
+        pub(crate) fn error_view<C>(&self, ctx: &Context<C>) -> Html
+        where
+            C: Component,
+            <C as Component>::Message: From<Msg>,
+        {
+            let show_details = self.show_error_details;
+            let link = ctx.link();
+            let toggle_details = link.callback(move |_| Msg::ShowErrorDetails(!show_details));
+            let clear = link.callback(|_| Msg::from(ClearErrors { _seal: () }));
+            html! {
+                <div>
+                    if self.errors.is_empty() {
+                        { "No Errors" }
+                    } else {
+                        <>
+                            { self.errors.len() }
+                            { " Errors " }
+                            <button onclick={toggle_details}>
+                                { if show_details { "Hide" } else { "Show" } }
+                            </button>
+                            { " " }
+                            <button onclick={clear}>{ "Clear" }</button>
+                            if show_details {
+                                { self.errors.iter().collect::<Html>() }
+                            }
+                        </>
+                    }
+                </div>
+            }
+        }
+        fn clear_errors(&mut self) -> bool {
+            let was_empty = self.errors.is_empty();
+            self.errors.clear();
+            !was_empty
+        }
+        fn set_show_error_details(&mut self, show: bool) -> bool {
+            let changed = self.show_error_details != show;
+            self.show_error_details = show;
+            changed
+        }
+    }
+    struct LogErrorEntry(&'static str, String);
+    impl<'a> FromIterator<&'a LogErrorEntry> for Html {
+        fn from_iter<T: IntoIterator<Item = &'a LogErrorEntry>>(iter: T) -> Self {
+            html! {
+                <ul class="errors">
+                    {
+                        iter.into_iter().enumerate().map(|(index, LogErrorEntry(ty, msg))| html! {
+                            <li key={index}>
+                                <b>{ty}</b>
+                                {" - "}
+                                { msg }
+                            </li>
+                        }).collect::<Html>()
+                    }
+                </ul>
+            }
+        }
+    }
+    impl<C> UpdateDelegate<C> for Logger
+    where
+        C: Component,
+    {
+        type Message = Msg;
+
+        fn update(&mut self, _ctx: &Context<C>, message: Self::Message) -> bool {
+            match message {
+                Msg::Message(message) => {
+                    log!("App got message {}", message);
+                    false
+                }
+                Msg::Error((error_ty, error_msg)) => {
+                    self.errors.push(LogErrorEntry(error_ty, error_msg));
+                    true
+                }
+                Msg::ClearErrors(..) => {
+                    let errors = self.clear_errors();
+                    let show = self.set_show_error_details(false);
+                    errors || show
+                }
+                Msg::ShowErrorDetails(show) => self.set_show_error_details(show),
+            }
+        }
     }
 }
 
+type WebsocketHandler = websocket::Handler<shared::ServerResponse, shared::ClientRequest>;
 struct App {
-    websocket: websocket::Simple,
+    websocket: WebsocketHandler,
     reconnect: reconnect::Logic<ExponentialBackoff<SystemClock>>,
-    logger: Logger,
+    logger: log::Logger,
 }
 impl App {
-    fn new_websocket(ctx: &Context<Self>) -> websocket::Simple {
+    fn new_websocket(ctx: &Context<Self>) -> WebsocketHandler {
         let url_websocket = {
             let location = web_sys::window().expect("window exists").location();
             let host = location.host().expect("window.location has host");
             format!("ws://{host}/ws")
         };
         let link = ctx.link();
-        let on_message = link.callback(MsgLog::Message);
-        let on_error = link.callback(|e| match e {
-            gloo_net::websocket::WebSocketError::ConnectionError
-            | gloo_net::websocket::WebSocketError::MessageSendError(_) => {
-                reconnect::Msg::ConnectionError.into()
+        let on_message =
+            link.callback(|server_response| log::Msg::Message(format!("{server_response:?}")));
+        let on_error = link.callback(|e| -> AppMsg {
+            const TYPE_WEBSOCKET: &str = "websocket";
+            const TYPE_SERDE: &str = "serde";
+            match e {
+                websocket::Error::WebSocket(e) => match e {
+                    WebSocketError::ConnectionError | WebSocketError::MessageSendError(_) => {
+                        reconnect::Msg::ConnectionError.into()
+                    }
+                    WebSocketError::ConnectionClose(_) => reconnect::Msg::ConnectionClose.into(),
+                    other => log::Msg::Error((
+                        TYPE_WEBSOCKET,
+                        format!("unknown WebSocketError: {other:?}"),
+                    ))
+                    .into(),
+                },
+                websocket::Error::UnexpectedBytes(bytes) => {
+                    log::Msg::Error((TYPE_WEBSOCKET, format!("unexpected bytes: {bytes:?}"))).into()
+                }
+                websocket::Error::SerdeJson(error) => {
+                    log::Msg::Error((TYPE_SERDE, format!("{error}"))).into()
+                }
             }
-            gloo_net::websocket::WebSocketError::ConnectionClose(_) => {
-                reconnect::Msg::ConnectionClose.into()
-            }
-            other => AppMsg::from(MsgLog::Message(format!(
-                "unknown WebSocketError: {other:?}"
-            ))),
         });
-        websocket::Simple::new(
+        websocket::Handler::new(
             url_websocket,
             websocket::Callbacks {
                 on_message,
@@ -169,13 +273,13 @@ impl Component for App {
         Self {
             websocket,
             reconnect,
-            logger: Logger,
+            logger: log::Logger::default(),
         }
     }
 
     fn update(&mut self, ctx: &Context<Self>, message: AppMsg) -> bool {
         if self.reconnect.get_timeout_millis().is_some() {
-            if let AppMsg::Logger(MsgLog::Message(..)) = &message {
+            if let AppMsg::Logger(log::Msg::Message(..)) = &message {
                 ctx.link()
                     .callback(|_| reconnect::Msg::ConnectionEstablished)
                     .emit(());
@@ -192,14 +296,17 @@ impl Component for App {
             <>
                 <header class="monospace">{ "soundbox-ii" }</header>
                 <div class="content">
-                    {"This is a websocket test"}
-                    <br/>
-                    {"Websocket "}
-                    <button onclick={websocket_connect}>{"Connect"}</button>
-                    <button onclick={websocket_disconnect}>{"Disconnect"}</button>
+                    <div>
+                        {"This is a websocket test"}
+                        <br/>
+                        {"Websocket "}
+                        <button onclick={websocket_connect}>{"Connect"}</button>
+                        <button onclick={websocket_disconnect}>{"Disconnect"}</button>
+                    </div>
                     <BrowserRouter>
                         <Switch<Route> render={Switch::render(switch_main)} />
                     </BrowserRouter>
+                    { self.logger.error_view(ctx) }
                 </div>
                 <footer>{ "(c) 2021-2022 - don't keep your sounds boxed up" }</footer>
             </>
@@ -217,29 +324,45 @@ mod websocket {
     use gloo::utils::errors::JsError;
     use gloo_net::websocket::{futures::WebSocket, Message, WebSocketError};
     use serde::{de::DeserializeOwned, Serialize};
+    use std::marker::PhantomData;
     use wasm_bindgen_futures::spawn_local;
     use yew::{Callback, Component, Context};
 
-    use crate::{macros::UpdateDelegate, App};
+    use crate::macros::UpdateDelegate;
 
     pub(crate) enum Msg {
         Connect,
         Disconnect,
     }
 
+    shared::wrapper_enum! {
+        #[derive(Debug)]
+        pub enum Error {
+            WebSocket(WebSocketError),
+            SerdeJson(serde_json::Error),
+            { impl None for }
+            UnexpectedBytes(Vec<u8>),
+        }
+    }
+
     type HandleWrite = SplitSink<WebSocket, Message>;
     type HandleRead = SplitStream<WebSocket>;
-    pub(crate) struct Simple {
+    pub(crate) struct Handler<T, U> {
         url: String,
         write_handle: Option<(HandleWrite, oneshot::Receiver<shared::Never>)>,
-        callbacks: Callbacks,
+        callbacks: Callbacks<T>,
+        _phantom: PhantomData<U>,
     }
-    impl Simple {
-        pub fn new(url: String, callbacks: Callbacks) -> Self {
+    impl<T, U> Handler<T, U>
+    where
+        T: DeserializeOwned + 'static,
+    {
+        pub fn new(url: String, callbacks: Callbacks<T>) -> Self {
             Self {
                 url,
                 callbacks,
                 write_handle: None,
+                _phantom: PhantomData,
             }
         }
         fn try_connect(&mut self) -> Result<(), JsError> {
@@ -248,37 +371,45 @@ mod websocket {
             let (read_loop_tx, read_loop_rx) = oneshot::channel();
             self.write_handle.replace((write, read_loop_rx));
             let callbacks = self.callbacks.clone();
-            spawn_local(async move {
-                if let Err(err) = callbacks.run_reader_loop(read, read_loop_tx).await {
-                    log!("read loop err {}", err);
-                }
-            });
+            spawn_local(callbacks.run_reader_loop(read, read_loop_tx));
             Ok(())
         }
     }
-    #[derive(Clone)]
-    pub struct Callbacks {
-        pub on_message: Callback<String>,
-        pub on_error: Callback<WebSocketError>,
+    pub struct Callbacks<T> {
+        pub on_message: Callback<T>,
+        pub on_error: Callback<Error>,
     }
-    impl Callbacks {
+    impl<T> Clone for Callbacks<T> {
+        fn clone(&self) -> Self {
+            Self {
+                on_message: self.on_message.clone(),
+                on_error: self.on_error.clone(),
+            }
+        }
+    }
+    impl<T> Callbacks<T>
+    where
+        T: DeserializeOwned,
+    {
         async fn run_reader_loop(
             self,
             mut read: HandleRead,
             mut shutdown: oneshot::Sender<shared::Never>,
-        ) -> Result<(), WebSocketError> {
+        ) {
             let mut shutdown = shutdown.cancellation().fuse();
             loop {
                 //TODO move outside the loop, per guidelines
                 let mut read = read.next().fuse();
                 select! {
                     read_result = read => match read_result {
-                        Some(Ok(message)) => self.handle_message(message),
-                        Some(Err(error)) => self.handle_error(error),
+                        Some(message) => match self.handle_message(message) {
+                            Ok(()) => {}
+                            Err(error) => self.handle_error(error),
+                        }
                         None => {
                             log!("websocket read loop: server hung up :/");
                             break;
-                        },
+                        }
                     },
                     _ = shutdown => {
                         log!("websocket read loop: shutdown accepted");
@@ -288,24 +419,30 @@ mod websocket {
             }
             // TODO code smell... how to `.fuse()` outside the loop (per docs), while still getting messages?
             std::mem::forget(read);
-            Ok(())
         }
-        fn handle_message(&self, message: gloo_net::websocket::Message) {
+        fn handle_message(&self, message: Result<Message, WebSocketError>) -> Result<(), Error> {
             // log!("websocket read: {message:?}");
-            match message {
-                Message::Text(text) => self.on_message.emit(text),
+            match message? {
+                Message::Text(text) => {
+                    let message = serde_json::from_str(&text)?;
+                    self.on_message.emit(message);
+                }
                 Message::Bytes(bytes) => log!(
                     "websocket read loop: unsupported input of bytes {:?}",
                     bytes
                 ),
             }
+            Ok(())
         }
-        fn handle_error(&self, error: WebSocketError) {
-            log!("websocket read error: {error:?}");
+        fn handle_error(&self, error: Error) {
+            log!("websocket error: {error:?}");
             self.on_error.emit(error);
         }
     }
-    impl<C: Component> UpdateDelegate<C> for Simple {
+    impl<C: Component, T, U> UpdateDelegate<C> for Handler<T, U>
+    where
+        T: DeserializeOwned + 'static,
+    {
         type Message = Msg;
         fn update(&mut self, _ctx: &Context<C>, message: Msg) -> bool {
             match message {
@@ -330,7 +467,7 @@ mod websocket {
 mod reconnect {
     use backoff::backoff::Backoff;
     use gloo_timers::callback::Timeout;
-    use yew::{Callback, Component};
+    use yew::{Callback, Component, Context};
 
     use crate::macros::UpdateDelegate;
 
@@ -371,7 +508,7 @@ mod reconnect {
         pub fn get_timeout_millis(&self) -> Option<u32> {
             self.timeout_millis.as_ref().map(|(_, millis)| *millis)
         }
-        /// Schedules the next reconnect timeout
+        /// Schedules the next connect timeout
         fn schedule_timeout(&mut self) {
             if self.timeout_millis.is_some() {
                 log!("reconnect: ignore schedule timeout, already scheduled");
@@ -381,9 +518,9 @@ mod reconnect {
                 log!("reconnect: schedule timeout for {delay:?}");
                 let delay_millis = delay.as_millis().try_into().unwrap_or(u32::MAX);
                 debug!("reconnect delay_millis = {}", delay_millis);
-                let reconnect = self.callbacks.connect.clone();
+                let connect = self.callbacks.connect.clone();
                 let timeout = Timeout::new(delay_millis, move || {
-                    reconnect.emit(());
+                    connect.emit(());
                 });
                 self.timeout_millis = Some((timeout, delay_millis));
             }
@@ -392,7 +529,7 @@ mod reconnect {
     impl<B: Backoff, C: Component> UpdateDelegate<C> for Logic<B> {
         type Message = Msg;
 
-        fn update(&mut self, _ctx: &yew::Context<C>, message: Self::Message) -> bool {
+        fn update(&mut self, _ctx: &Context<C>, message: Self::Message) -> bool {
             match message {
                 Msg::ConnectionEstablished => self.reset_all(), // DONE
                 Msg::ConnectionClose => self.schedule_timeout(), // only if needed, Retry (??)
