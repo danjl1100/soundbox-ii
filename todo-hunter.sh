3edacaabef773ce974617ffbba2b1b98c7386cea
Daniel Lambert <danjl1100@gmail.com>
2023-01-08 14:33:00 -0600

    add todo-hunter script

---
+++ todo-hunter.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p gnugrep
+
+# not-so-clever evasion tactic to exclude this script from the search results
+NEEDLE="TO"
+NEEDLE="${NEEDLE}DO"
+
+find . -type f -not -path './.git/*' -not -path '*/target/*' -not -path './dist/*' | xargs grep -i "${NEEDLE}" --color
