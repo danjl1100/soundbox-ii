9a3e795455f698b65b427891ac3ba44bc592b721
Daniel Lambert <danjl1100@gmail.com>
2024-08-13 18:15:15 -0500

    prep order for extracting submodule files

--- bucket-spigot/src/order.rs
+++ bucket-spigot/src/order.rs
@@ -180,66 +180,69 @@ pub struct PeekAccepted {
     new_root_order: Root,
 }
 
-// `Option<Lazy<T>>` seems cleaner and more meaningful than `Option<Option<T>>`
-// (heeding advice from the pedantic lint `clippy::option_option`)
-#[derive(Clone, Default)]
-enum Lazy<T> {
-    Value(T),
-    #[default]
-    Uninit,
-}
-impl<T> Lazy<T> {
-    fn as_mut(&mut self) -> Option<&mut T> {
-        match self {
-            Self::Value(value) => Some(value),
-            Self::Uninit => None,
-        }
+use counts_remaining::CountsRemaining;
+mod counts_remaining {
+    // `Option<Lazy<T>>` seems cleaner and more meaningful than `Option<Option<T>>`
+    // (heeding advice from the pedantic lint `clippy::option_option`)
+    #[derive(Clone, Default)]
+    pub(super) enum Lazy<T> {
+        Value(T),
+        #[default]
+        Uninit,
     }
-    fn as_mut_or_init(&mut self, init_fn: impl FnOnce() -> T) -> &mut T {
-        match self {
-            Self::Value(_) => {}
-            Self::Uninit => {
-                *self = Self::Value(init_fn());
+    impl<T> Lazy<T> {
+        fn as_mut(&mut self) -> Option<&mut T> {
+            match self {
+                Self::Value(value) => Some(value),
+                Self::Uninit => None,
             }
         }
-        self.as_mut().expect("should initialize directly above")
-    }
-}
-
-#[derive(Clone)]
-struct CountsRemaining(Vec<Option<Lazy<Self>>>);
-impl CountsRemaining {
-    fn new(len: usize) -> Self {
-        Self(vec![Some(Lazy::default()); len])
-    }
-    /// # Panics
-    /// Panics if the index is out of bounds (greater than `len` provided in [`Self::new`]),
-    /// or all children are exhausted.
-    fn set_empty(&mut self, index: usize) {
-        self.0[index].take();
-
-        // check if all are exhausted
-        if self.0.iter().all(Option::is_none) {
-            // ensure any future calls error (loudly)
-            self.0.clear();
+        pub fn as_mut_or_init(&mut self, init_fn: impl FnOnce() -> T) -> &mut T {
+            match self {
+                Self::Value(_) => {}
+                Self::Uninit => {
+                    *self = Self::Value(init_fn());
+                }
+            }
+            self.as_mut().expect("should initialize directly above")
         }
     }
-    /// Returns a mutable reference to the child's remaining count (which may not yet be
-    /// initialized) or `None` if the child is exhausted (e.g. via [`Self::set_empty`])
-    ///
-    /// # Panics
-    /// Panics if the index is out of bounds (greater than `len` provided in [`Self::new`]),
-    /// or all children are exhausted.
-    fn child_mut(&mut self, index: usize) -> Option<&mut Lazy<Self>> {
-        self.0[index].as_mut()
-    }
-    /// Returns true if all children are exhausted
-    fn is_fully_exhausted(&self) -> bool {
-        self.0.is_empty()
-    }
-    /// Returns the number of children, or `0` if all children are exhausted
-    fn child_count_if_nonempty(&self) -> usize {
-        self.0.len()
+
+    #[derive(Clone)]
+    pub(super) struct CountsRemaining(Vec<Option<Lazy<Self>>>);
+    impl CountsRemaining {
+        pub fn new(len: usize) -> Self {
+            Self(vec![Some(Lazy::default()); len])
+        }
+        /// # Panics
+        /// Panics if the index is out of bounds (greater than `len` provided in [`Self::new`]),
+        /// or all children are exhausted.
+        pub fn set_empty(&mut self, index: usize) {
+            self.0[index].take();
+
+            // check if all are exhausted
+            if self.0.iter().all(Option::is_none) {
+                // ensure any future calls error (loudly)
+                self.0.clear();
+            }
+        }
+        /// Returns a mutable reference to the child's remaining count (which may not yet be
+        /// initialized) or `None` if the child is exhausted (e.g. via [`Self::set_empty`])
+        ///
+        /// # Panics
+        /// Panics if the index is out of bounds (greater than `len` provided in [`Self::new`]),
+        /// or all children are exhausted.
+        pub fn child_mut(&mut self, index: usize) -> Option<&mut Lazy<Self>> {
+            self.0[index].as_mut()
+        }
+        /// Returns true if all children are exhausted
+        pub fn is_fully_exhausted(&self) -> bool {
+            self.0.is_empty()
+        }
+        /// Returns the number of children, or `0` if all children are exhausted
+        pub fn child_count_if_nonempty(&self) -> usize {
+            self.0.len()
+        }
     }
 }
 
