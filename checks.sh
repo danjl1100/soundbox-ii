94a688fd1caffba00ce77d4402fb182891588169
Daniel Lambert <danjl1100@gmail.com>
2025-09-20 19:27:15 -0500

    demonstrate rendering HTML elements

--- checks.sh
+++ checks.sh
@@ -14,7 +14,7 @@ cd "$(git rev-parse --show-toplevel)"
 true \
   && echo "Missing copyright notice in changed files:" \
     && [[ ! $( \
-        git diff --cached --name-only HEAD | grep '.rs$' | \
+        git diff --cached --name-only HEAD | grep '.\(rs\|ts\)$' | \
         xargs --no-run-if-empty grep -LH "${COPYRIGHT_TEXT}" | tee /dev/stderr \
       ) ]] \
       || (echo "fix using:   echo \"// ${COPYRIGHT_TEXT}, see /COPYING file for details
@@ -22,6 +22,7 @@ true \
     && echo "[none]" \
   && (echo "3972dc9744f6499f0f9b2dbf76696f2ae7ad8af9b23dde66d6af86c9dfb36986  COPYING" | sha256sum -c - --strict) \
   && echo "Outstanding cargo fmt files:" && cargo fmt --all -- --check -l && echo "[none]" \
+  && cargo xtask spigot-visual-dist \
   && cargo clippy --workspace --all-targets --color always \
   && cargo test --workspace --color always \
   && cargo doc --workspace --no-deps -q --color always \
--- spigot-visual/static-ts/app.ts
+++ spigot-visual/static-ts/app.ts
@@ -1,7 +1,16 @@
-async function do_something() {
-  console.log("hey");
+// Copyright (C) 2021-2025  Daniel Lambert. Licensed under GPL-3.0-or-later, see /COPYING file for details
+
+async function render_spigot(container: HTMLElement) {
+  container.innerHTML = '';
+
+  const placeholder = document.createElement('p');
+  placeholder.innerText = 'Dynamic content from app.ts!';
+  container.appendChild(placeholder);
 }
 
 window.onload = async (): Promise<void> => {
-  await do_something();
+  const content = document.getElementById('content');
+  if (content != null) {
+    await render_spigot(content);
+  }
 };
