2b50266e79a2bf6c11b75f05a104fb0f56937557
Daniel Lambert <danjl1100@gmail.com>
2026-07-28 18:17:24 -0500

    xtask fixup fake-vlc exit sequencing, env hints

--- crates/xtask/src/beet_pusher.rs
+++ crates/xtask/src/beet_pusher.rs
@@ -3,7 +3,7 @@
 
 use eyre::Context as _;
 
-use crate::{TypedResult, status_cmd::spawn_cmd_try_args};
+use crate::{CmdSettings, TypedResult, status_cmd::spawn_cmd_try_args};
 
 /// Spawns the `beet-pusher-webui` server along with the backend `beet-pusher`
 /// with `fake-beet`
@@ -35,17 +35,14 @@ impl WebUiSpawn {
 
         let cmd_settings = cmd_args.into_inner();
 
-        cmd_settings.run_cargo(|c| {
-            c.args([
-                "build",
-                "--bin",
-                "beet-pusher",
-                "--bin",
-                "beet-pusher-webui",
-                "--bin",
-                "beet-pusher-webui-spawn",
-            ])
-        })?;
+        let Binaries {
+            beet_pusher,
+            beet_pusher_webui,
+            beet_pusher_webui_spawn,
+        } = Binaries::build(&cmd_settings)?;
+
+        let dir = tempfile::tempdir().context("failed to create tempdir for config files")?;
+        let dir = dir.path();
 
         let args = ArgsCombined::from(args);
 
@@ -57,22 +54,6 @@ impl WebUiSpawn {
             simulate_beet,
         } = args;
 
-        let dir = tempfile::tempdir().context("failed to create tempdir for config files")?;
-        let dir = dir.path();
-
-        let build_bin = |bin_name| {
-            escargot::CargoBuild::new()
-                .bin(bin_name)
-                .run()
-                .with_context(|| format!("failed to build {bin_name}"))
-        };
-
-        let beet_pusher = build_bin("beet-pusher")?;
-        let beet_pusher_webui = build_bin("beet-pusher-webui")?;
-        let beet_pusher_webui_spawn = build_bin("beet-pusher-webui-spawn")?;
-
-        eprintln!("Built all cargo bins");
-
         let fake_beet_and_config = simulate_beet
             .then(|| -> eyre::Result<_> {
                 let fake_beet = fake_beet::try_build_bin_once()
@@ -80,7 +61,6 @@ impl WebUiSpawn {
                     .context("failed to build fake-beet")?;
 
                 let config = gen_fake_beet_config(dir)?;
-
                 eprintln!("Created fake-beet config");
 
                 Ok((fake_beet, config))
@@ -90,9 +70,10 @@ impl WebUiSpawn {
         let (vlc_auth_file, fake_vlc_and_thread) = simulate_vlc
             .then(|| -> eyre::Result<_> {
                 let (fake_vlc, thread) = ::fake_vlc::FakeVlc::new()?;
-                let vlc_auth_file = fake_vlc.create_config_file(dir, "fake-vlc.toml")?;
-
                 eprintln!("Spawned fake-vlc");
+
+                let vlc_auth_file = fake_vlc.create_config_file(dir, "fake-vlc.toml")?;
+                eprintln!("Created fake-vlc config: {}", vlc_auth_file.display());
 
                 Ok((vlc_auth_file, (fake_vlc, thread)))
             })
@@ -110,7 +91,7 @@ impl WebUiSpawn {
                         .env("FAKE_BEET_CONFIG_FILE", config);
                 }
 
-                if let Some(vlc_auth_file) = vlc_auth_file {
+                if let Some(vlc_auth_file) = &vlc_auth_file {
                     cmd.env("VLC_AUTH_FILE", vlc_auth_file);
                 }
 
@@ -143,14 +124,14 @@ impl WebUiSpawn {
 
                 Ok(cmd)
             },
-        )?
-        .map_err(super::status_cmd::SpawnFail::into_eyre_in_final_main_error_report_location)?;
+        )??;
 
         #[allow(
             clippy::missing_panics_doc,
             reason = "passthru panic from spawned thread"
         )]
-        if let Some((_fake_vlc, thread)) = fake_vlc_and_thread {
+        if let Some((fake_vlc, thread)) = fake_vlc_and_thread {
+            drop(fake_vlc);
             thread
                 .join()
                 .expect("fake_vlc thread panic")
@@ -158,6 +139,47 @@ impl WebUiSpawn {
         }
 
         Ok(())
+    }
+}
+
+#[expect(clippy::struct_field_names, reason = "bin names have a common prefix")]
+struct Binaries {
+    beet_pusher: escargot::CargoRun,
+    beet_pusher_webui: escargot::CargoRun,
+    beet_pusher_webui_spawn: escargot::CargoRun,
+}
+impl Binaries {
+    fn build(cmd_settings: &CmdSettings) -> TypedResult<Self> {
+        // `cargo build` all first, for consolidated error messages
+        cmd_settings.run_cargo(|c| {
+            c.args([
+                "build",
+                "--bin",
+                "beet-pusher",
+                "--bin",
+                "beet-pusher-webui",
+                "--bin",
+                "beet-pusher-webui-spawn",
+            ])
+        })?;
+
+        let build_bin = |bin_name| {
+            escargot::CargoBuild::new()
+                .bin(bin_name)
+                .run()
+                .with_context(|| format!("failed to build {bin_name}"))
+        };
+
+        // build each again (should be cached), for easy path discovery
+        let beet_pusher = build_bin("beet-pusher")?;
+        let beet_pusher_webui = build_bin("beet-pusher-webui")?;
+        let beet_pusher_webui_spawn = build_bin("beet-pusher-webui-spawn")?;
+
+        Ok(Self {
+            beet_pusher,
+            beet_pusher_webui,
+            beet_pusher_webui_spawn,
+        })
     }
 }
 
@@ -211,9 +233,9 @@ impl std::fmt::Display for ArgsCombined {
 
         if simulate_vlc {
             write!(f, "- simulating VLC")?;
-            hint_line(f, "VLC_AUTH_FILE", "load a VLC config TOML file")?;
         } else {
             write!(f, "- using external VLC with provided config")?;
+            hint_line(f, "VLC_AUTH_FILE", "load a VLC config TOML file")?;
             hint_line(
                 f,
                 "--simulate-vlc",
